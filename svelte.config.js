1c5553e9cd05c8a838f6fa7155683bbc23b56c68
urtreebro <acespadesdamn@bk.ru>
2025-06-11 23:24:27 +0300

    listen to sabrina carpenter manchild plsss its soooo good

--- svelte.config.js
+++ svelte.config.js
@@ -6,14 +6,15 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	csrf: {
-		checkOrigin: false,
-	},
+
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		csrf: {
+			checkOrigin: false,
+		},
 	}
 };
 
